9962777637f83856afa4546c0707d1a55ddc5b66
CBshmear <connorbeer13@gmail.com>
2023-02-08 17:29:49 -0600

    added procfile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,6 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -17,7 +16,6 @@ router.get("/", async (req, res) => {
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render("homepage", {
       posts,
       logged_in: req.session.logged_in,
